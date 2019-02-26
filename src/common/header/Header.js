2c9152c8bcbd4c1d6ad7e75f053476f38408ca62
anupriyakhare34 <anupriya.khare34@gmail.com>
2019-02-26 17:50:47 +0530

    Added header as image viewer
    Added header as image viewer

---
+++ src/common/header/Header.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './Header.css';
+
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+    root: {
+      flexGrow: 1,
+    },
+  };
+
+  function SimpleAppBar(props) {
+    const { classes } = props;
+
+  class Header extends Component {
+    render(){
+  
+  
+    return (
+      <header className="app-header">
+      <div className={classes.root}>
+        <AppBar position="static" color="#default">
+          <Toolbar>
+            <Typography variant="h6" color="inherit">
+              Image Viewer
+            </Typography>
+          </Toolbar>       
+        </AppBar>
+      </div>
+      </header>
+    );
+  }
+}
+  
+  SimpleAppBar.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
+  
+  export default withStyles(styles)(SimpleAppBar);
+  export default Header;
