805c5c392b8e6fb62ad3dd1123b9f88ed789f546
kritijain2110 <43193916+kritijain2110@users.noreply.github.com>
2019-03-03 02:04:56 +0530

    Corrected Index.js file to load Login Page at Application StartUp

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Profile from './screens/profile/profile';
+//import Profile from './screens/profile/profile';
+import Login from './screens/login/login'
 
-ReactDOM.render(<Profile/>, document.getElementById('root'));
+//ReactDOM.render(<Profile/>, document.getElementById('root'));
+ReactDOM.render(<Login/>, document.getElementById('root'));
 
