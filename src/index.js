8103d49885ccc22d29b9650a8504f7c8387f6976
kritijain2110 <43193916+kritijain2110@users.noreply.github.com>
2019-03-03 22:14:54 +0530

    Final Commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Login from './screens/login/login'
+import Login from './screens/login/Login'
 
 
 ReactDOM.render(<Login/>, document.getElementById('root'));
