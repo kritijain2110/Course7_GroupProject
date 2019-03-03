ec9141fc86439271cd93866a6214aa6da0da27fa
kritijain2110 <43193916+kritijain2110@users.noreply.github.com>
2019-03-03 21:10:44 +0530

    Added functionality to display image information using APIs

--- src/screens/profile/PostdetailSection.js
+++ src/screens/profile/PostdetailSection.js
@@ -3,19 +3,44 @@ import './PostdetailSection.css';
 import Typography from '@material-ui/core/Typography';
 
 class PostdetailSection extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            standard_resolution:"",
+            profile_picture:"",
+            username:"",
+            caption:"",
+            tags:[],
+            comments:[],
+            likes:""
+
+        }
+    }
+    componentWillMount(){
+        this.setState({
+            standard_resolution:this.props.currentPostData.images.standard_resolution.url,
+            profile_picture:this.props.currentPostData.user.profile_picture,
+            username:this.props.currentPostData.user.username,
+            caption:this.props.currentPostData.caption.text.split('\n')[0],
+            tags:this.props.currentPostData.tags,
+            likes:this.props.currentPostData.likes.count
+
+        })
+        console.log(this.state.standard_resolution);
+    }
     render(){
         return(
-            <div className="postDetail">
+            <div className="postDetailsContainer">
                 <div className="left">
-                    <img className="postedImage" src="" />
+                    <img className="postedImage" src={this.state.standard_resolution} alt={this.state.username} />
                 </div>
                 <div className="right">
                     <div className="userData">
                         <div>
-                            <img className="profileImageIcon" src="" />
+                            <img className="profileImageIcon" src={this.state.profile_picture} alt={this.state.username} />
                         </div>
                         <div>
-                            <Typography variant="h5" component="h5">UserName </Typography>
+                            <Typography variant="h5" component="h5">{this.state.username} </Typography>
                         </div>
 
                     </div>
