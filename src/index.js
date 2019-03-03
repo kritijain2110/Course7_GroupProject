be2a0e6dd23f284255b9b00f5653cb9d4a43561b
kritijain2110 <43193916+kritijain2110@users.noreply.github.com>
2019-03-03 21:43:51 +0530

    Fixed index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Profile from './screens/profile/Profile';
-//import Login from './screens/login/login'
+import Login from './screens/login/login'
 
-ReactDOM.render(<Profile/>, document.getElementById('root'));
-//ReactDOM.render(<Login/>, document.getElementById('root'));
+
+ReactDOM.render(<Login/>, document.getElementById('root'));
 
